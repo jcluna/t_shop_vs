6ed64135cebe1573a43d6c33a8e077be81e7a8f3
Juan C. Luna H. <jcluna@theos.com.mx>
2023-10-16 15:19:09 -0600

    Definición de constantes para APIs

---
+++ lib/utils/theme/constants/api_constants.dart
@@ -0,0 +1,3 @@
+/* -- List of constants used in APIs -- */
+const String tSecretAPIKey =
+    "A9Nzu9JMsdwzVvt3BtdXu0imxgyIRFYrgltUTqQzJ9ZjJrN8a9DRme8XglLOccM5vONvLajRO2r2JmKbq8Xhwe5zRXl2ApGW7tRQ8KYFtKfjTUTm2Tb0XRxshPg5ZBey";
